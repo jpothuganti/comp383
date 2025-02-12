773268807be3a24cc245bd86d4fa40ab390400df
Jay Pothuganti <jpothuganti@luc.edu>
2025-02-12 16:09:49 -0600

    committed

--- DNA.py
+++ DNA.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
+s = open('rosalind_dna.txt','r').read()
 for n in ["A","C","G","T"]:
-    print(s.count(n), end=' ')s = open('rosalind_dna.txt','r').read()
+    print(s.count(n), end=' ')
